c335ea1416ada250ad52c13756fc4a4e71e20e6e
Cristian Camargo <cristian.camargo@outlook.com.br>
2018-01-30 12:30:40 -0200

    Atualização

--- Php 7 - Udemy/Modulo-08 - PHP - DateTime/Aula_01.php
+++ Php 7 - Udemy/Modulo-08 - PHP - DateTime/Aula_01.php
@@ -1,4 +1,12 @@
 <?php
+// 01/01/1970 Timestamp
+    echo date('d/m/Y') . "<br />";
+    echo time() . "<br />";
+    echo "<br /> -------------------------- <br />";
 
+    $Time = strtotime("2001-09-11");
+    $Time = strtotime("now");
+    $Time = strtotime("+1 week");
 
+    echo date("l, d/m/Y", $Time);
 ?>

b27df394853deacc5d5515d811ef5d249e6f6adf
Cristian Camargo <cristian.camargo@outlook.com.br>
2018-01-31 02:46:03 -0200

    Aula de Herança.

--- Php 7 - Udemy/Modulo-09 - PHP - POO/Aula_07.php
+++ Php 7 - Udemy/Modulo-09 - PHP - POO/Aula_07.php
@@ -1,5 +1,31 @@
 <?php
 
+    class Document {
+        private $Numero;
+
+        public function getNumero() {
+            return $this -> Numero;
+        }
+
+        public function setNumero($Numero) {
+            $this -> Numero = $Numero;
+        }
+    }
 
 
+    class CPF extends Document {
+        public function Validar():bool {
+            $NumeroCPF = $this -> getNumero();
+            
+            // Aqui Validação do CPF
+            return true;
+        }
+    }
+
+
+    $Doc = new CPF();
+    $Doc -> setNumero("09492594978");
+    var_dump($Doc -> Validar());
+    echo "<br />";
+    echo $Doc -> getNumero();
 ?>

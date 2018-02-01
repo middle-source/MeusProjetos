b62dd4094ea8f4e91f531b92cdb2695b57b495d4
Cristian Camargo <cristian.camargo@outlook.com.br>
2018-02-01 00:12:55 -0200

    aulas integrando com banco de dados

---
+++ Php 7 - Udemy/Modulo-10 - PHP - Banco de Dados/query.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE curso_hcode;
+USE curso_hcode;
+
+CREATE TABLE tb_usuarios(
+	id_usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+    deslogin VARCHAR(64) NOT NULL, 
+    dessenha VARCHAR(256) NOT NULL, 
+    dtcadastro TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()
+);
+
+
+INSERT INTO tb_usuarios(deslogin, dessenha) VALUES( 'root', '123456');
+
+SELECT * FROM tb_usuarios;
+
+UPDATE tb_usuarios SET dessenha = '1234567' WHERE id_usuario = 1;
+
+DELETE FROM tb_usuarios WHERE id_usuario = 1;
+
+SELECT * FROM tb_usuarios;
+
+TRUNCATE TABLE tb_usuarios;

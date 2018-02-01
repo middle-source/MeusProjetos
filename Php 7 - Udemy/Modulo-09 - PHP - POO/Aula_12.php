087c20710e360fd0be3e63fc4601f581cde3611f
Cristian Camargo <cristian.camargo@outlook.com.br>
2018-01-31 23:44:32 -0200

    Testando o namespace

--- Php 7 - Udemy/Modulo-09 - PHP - POO/Aula_12.php
+++ Php 7 - Udemy/Modulo-09 - PHP - POO/Aula_12.php
@@ -1,4 +1,12 @@
 <?php
 
+    require_once('config.php');
+
+    $obj = new Cadastro();
+    $obj -> setNome("Allan Cristian");
+    $obj -> setEmail("allan.cristian@outlook.com.br");
+    $obj -> setSenha("123456");
+
+    echo $obj;
 
 ?>
---
+++ Php 7 - Udemy/Modulo-09 - PHP - POO/config.php
@@ -0,0 +1,12 @@
+<?php
+// Enteroperavel
+    spl_autoload_register(function($nameClass){
+        $dirClass = "class";
+        $fileName = $dirClass . DIRECTORY_SEPARATOR . $nameClass . ".class.php";
+
+        if(file_exists($fileName)) {
+            require_once($fileName);
+        }
+    });
+
+?>

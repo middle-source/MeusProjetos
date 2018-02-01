3b3005bf72be7e25d15cfee331bfffb65c376f91
Cristian Camargo <cristian.camargo@outlook.com.br>
2018-01-31 23:51:30 -0200

    Fim aulas POO

--- Php 7 - Udemy/Modulo-09 - PHP - POO/Aula_12.php
+++ Php 7 - Udemy/Modulo-09 - PHP - POO/Aula_12.php
@@ -2,11 +2,13 @@
 
     require_once('config.php');
 
+    use Cliente\Cadastro;
+
     $obj = new Cadastro();
     $obj -> setNome("Allan Cristian");
     $obj -> setEmail("allan.cristian@outlook.com.br");
     $obj -> setSenha("123456");
 
-    echo $obj;
+    $obj -> registrarVenda() ;
 
 ?>
--- Php 7 - Udemy/Modulo-09 - PHP - POO/config.php
+++ Php 7 - Udemy/Modulo-09 - PHP - POO/config.php
@@ -3,7 +3,6 @@
     spl_autoload_register(function($nameClass){
         $dirClass = "class";
         $fileName = $dirClass . DIRECTORY_SEPARATOR . $nameClass . ".class.php";
-
         if(file_exists($fileName)) {
             require_once($fileName);
         }

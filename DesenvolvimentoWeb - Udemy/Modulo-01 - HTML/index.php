d1690585ca4f4041e3eaf01775385b06e3f500a1
Cristian Camargo <cristian.camargo@outlook.com.br>
2018-02-01 15:39:38 -0200

    Atualização dos cursos

---
+++ DesenvolvimentoWeb - Udemy/Modulo-01 - HTML/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8" />
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+        <meta name = "X-UA-Compatible" content = "ie=edge" />
+        <title>Modulo 01 - HTML</title>
+    </head>
+    <body>
+    
+    
+    </body>
+</html>

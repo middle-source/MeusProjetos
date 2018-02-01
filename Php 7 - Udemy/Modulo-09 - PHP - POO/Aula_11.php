fffac00a6af93dd09959c5b16867c319c7a35b19
Cristian Camargo <cristian.camargo@outlook.com.br>
2018-01-31 22:59:12 -0200

    Fim Aula __autoload

--- Php 7 - Udemy/Modulo-09 - PHP - POO/Aula_11.php
+++ Php 7 - Udemy/Modulo-09 - PHP - POO/Aula_11.php
@@ -7,4 +7,19 @@
 
     $Carro = new DelRay;
     $Carro -> Acelerar(200);
+
+    echo "<br />";
+/* ------------------------------ */
+
+function incluirClasses($nomeClasse) {
+    if(file_exists("." . DIRECTORY_SEPARATOR . "class". DIRECTORY_SEPARATOR . $nomeClasse . ".class.php" ) == true ) {
+        require_once(".". DIRECTORY_SEPARATOR . "class" . DIRECTORY_SEPARATOR . $nomeClasse . ".class.php");
+    }
+}
+
+spl_autoload_register("incluirClasses");
+
+$Carro2 = new DelRay();
+$Carro2 -> Acelerar(290);
+
 ?>

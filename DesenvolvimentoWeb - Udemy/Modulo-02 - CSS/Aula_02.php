9b2abcef86498fc2ff02b7e498d9999564a48c88
= <cristian.camargo@outlook.com.br>
2018-02-01 19:21:38 -0200

    Atualização aula 03 Desenvolvimento Udemy

---
+++ DesenvolvimentoWeb - Udemy/Modulo-02 - CSS/Aula_02.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8" />
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+        <meta name = "X-UA-Compatible" content = "ie=edge" />
+        <title>Aula 02</title>
+        <style type = "text/css">
+            p{color: blue; }
+        </style>
+    </head>
+    <body>
+     <h1>Estilo CSS (Cascading Style Sheets) </h1>
+        <p>
+            Lorem ipsum dolor sit amet, facete eligendi ponderum nec ei, usu no homero omnesque. Liber fastidii petentium eu mei, vel ex nonumy tractatos consequuntur. Vim cu inani volumus sapientem, cibo zril vituperatoribus te nam, sed at tempor recusabo. Eu appetere corrumpit voluptaria per. Lorem ipsum dolor sit amet, facete eligendi ponderum nec ei, usu no homero omnesque. Liber fastidii petentium eu mei, vel ex nonumy tractatos consequuntur. Vim cu inani volumus sapientem, cibo zril vituperatoribus te nam, sed at tempor recusabo. Eu appetere corrumpit voluptaria per. Per at sonet prompta expetenda, ne mucius argumentum vel, eu vel natum labores. Per at sonet prompta expetenda, ne mucius argumentum vel, eu vel natum labores.
+        </p>
+
+        <p>
+            Nam cu facete alterum Lorem ipsum dolor sit amet, facete eligendi ponderum nec ei, usu no homero omnesque. Liber fastidii petentium eu mei, vel ex nonumy tractatos consequuntur. Vim cu inani volumus sapientem, cibo zril vituperatoribus te nam, sed at tempor recusabo. Eu appetere corrumpit voluptaria per. Per at sonet prompta expetenda, ne mucius argumentum vel, eu vel natum labores. assueverit, cu atqui feugiat deterruisset pri, vis ad wisi euismod sapientem. Pri debet interesset ut, ne mucius volutpat qui. Mea alterum equidem erroribus an, brute necessitatibus quo ad. Partem assentior est in, iudico soleat ea eum. Menandri electram accommodare et vix, id dico singulis aliquando qui.
+        </p>
+        
+    </body>
+</html>
---
+++ DesenvolvimentoWeb - Udemy/Modulo-02 - CSS/Aula_03.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset = "utf-8" />
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0" />
+        <meta name = "X-UA-Compatible" content = "ie=edge" />
+        <title>Aula 03 - SPAN DIV</title>
+        <style type = "text/css">
+
+        </style>
+    </head>
+    <body>
+    <div>
+    
+    
+    </div>
+        
+    </body>
+</html>

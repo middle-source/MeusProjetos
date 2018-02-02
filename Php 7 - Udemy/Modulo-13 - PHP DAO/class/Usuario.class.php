63ad10c8e2c6ba1cd93fa0f84e309d824a48820f
= <cristian.camargo@outlook.com.br>
2018-02-02 02:21:43 -0200

    Atualizando projeto

--- Php 7 - Udemy/Modulo-13 - PHP DAO/class/Usuario.class.php
+++ Php 7 - Udemy/Modulo-13 - PHP DAO/class/Usuario.class.php
@@ -1,7 +1,93 @@
 <?php
 
-    class Usuario extends Conn {
+    class Usuario {
+        /* Atributos */
+            private $idUsuario;
+            private $desLogin;
+            private $desSenha;
+            private $dtCadastro;
+        /* Fim Atributos */
         
+        /* Getters e Setters do idUsuario */
+
+            public function getIdusuario() {
+                return $this -> idUsuario;
+            }
+        
+            public function setIdusuario($idUsuario) {
+                $this -> idUsuario = $idUsuario;
+            }
+
+        /* Fim Getters e Setters do idUsuario */
+
+        /* Getters e Setters do desLogin */
+
+            public function getDeslogin() {
+                return $this -> desLogin;
+            }
+            public function setDeslogin($desLogin) {
+                $this -> desLogin = $desLogin;
+            }
+
+        /* Fim Getters e Setters do desLogin */
+
+        /* Getters e Setters do desSenha */
+            public function getDessenha() {
+                return $this -> desSenha;
+            }
+            public function setDessenha($desSenha) {
+                $this -> desSenha = $desSenha;
+            }
+        /* Fim Getters e Setters do desSenha */
+
+        /* Getters e Setters do dtCadastro */
+
+            public function getDtcadastro() {
+                return $this -> dtCadastro;
+            }
+
+            public function setDtcadastro($dtCadastro) {
+                $this -> dtCadastro = $dtCadastro;
+            }
+        /* Fim Getters e Setters do dtCadastro */
+
+    public function loadById($id) {
+        $sql = new Conn();
+        $results = $sql -> select("SELECT * FROM tb_usuarios WHERE id_usuario = :id", array(
+            ':id' => $id
+        ));
+
+
+        if(count($results) > 0) {
+            $row = $results[0];
+            $this -> setIdusuario($row['id_usuario']);
+            $this -> setDeslogin($row['deslogin']);
+            $this -> setDessenha($row['dessenha']);
+            $this -> setDtcadastro($row['dtcadastro']);
+        }
+    }
+
+
+    public function __toString() {
+        try{
+            
+            return json_encode(array(
+                "id_usuario" => $this -> getIdusuario(),
+                "deslogin" => $this -> getDeslogin(),
+                "dessenha" => $this -> getDessenha(),
+                "dtcadastro" => $this -> getDtcadastro()
+            ));
+
+        } catch(Exception $e) {
+            return 'Exceção capturada: '. $e -> getMessage() ;
+        }
+    }
+
+            
+    
+    
+    
+    
     }
 
 
--- Php 7 - Udemy/Modulo-13 - PHP DAO/index.php
+++ Php 7 - Udemy/Modulo-13 - PHP DAO/index.php
@@ -1,9 +1,13 @@
 <?php
     require_once('config.php');
 
-    $Conn = new Conn;
-    $usuarios = $Conn -> select("SELECT * FROM tb_usuarios");
+    // $Conn = new Conn;
+    // $usuarios = $Conn -> select("SELECT * FROM tb_usuarios");
 
-    echo json_encode($usuarios);
+    // echo json_encode($usuarios);
 
+    $usuario = new Usuario();
+    $usuario -> loadById(7);
+
+    echo $usuario;
 ?>

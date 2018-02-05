3d5abbfadc32a619e21bd821d9fb423fa70b48fe
= <cristian.camargo@outlook.com.br>
2018-02-05 00:14:08 -0200

    Aula de manipulação de diretorio

--- Php 7 - Udemy/Modulo-14 - PHP - ARQUIVOS/Aula_01.php
+++ Php 7 - Udemy/Modulo-14 - PHP - ARQUIVOS/Aula_01.php
@@ -1,21 +1,41 @@
 <?php
 
-    $name = "img";
-    /* Verificando se um diretorio existe e criando o mesmo */
+    
+    /* EXEMPLO 01 */
+        /* $name = "img"; */
+        /* Verificando se um diretorio existe e criando o mesmo */
+        
+            // if(!is_dir($name)) {
+            //     mkdir($name);
+            //     echo "Diretorio {$name} Criado com sucesso ! ";
+            // } else {
+            //    // echo "Diretorio {$name} já existe ! ";
+            //     rmdir($name);
+            //     echo "Já existe diretorio {$name} e já foi removido ! ";
+            // }
+
+
+
+        /* Fim Verificando se um diretorio existe e criando o mesmo */
+    /* FIM EXEMPLO 01 */
+
+    /* EXEMPLO 02 */
+        $images = scandir("img");
+       $data = array();
+        foreach ($images as $key) {
+            if(!in_array($key, array(".", ".."))) {
+                $filename = "img" . DIRECTORY_SEPARATOR . $key;
+                $info = pathinfo($filename);
+                $info["size"] = filesize($filename);
+                $info["modified"] = date("d/m/Y H:i:s", filemtime($filename));
+                $info["url"] = "https://localhost/cursos/Php%207%20-%20Udemy/Modulo-14%20-%20PHP%20-%20ARQUIVOS/";
+                array_push($data, $info);
+            }
+        }
        
-        // if(!is_dir($name)) {
-        //     mkdir($name);
-        //     echo "Diretorio {$name} Criado com sucesso ! ";
-        // } else {
-        //    // echo "Diretorio {$name} já existe ! ";
-        //     rmdir($name);
-        //     echo "Já existe diretorio {$name} e já foi removido ! ";
-        // }
+        // var_dump($images);
+echo json_encode($data);
 
-
-
-    /* Fim Verificando se um diretorio existe e criando o mesmo */
-
-
+    /* FIM EXEMPLO 02 */
 
 ?>

a97d234eb0743d517dfbbc00004416b794a56d6a
= <cristian.camargo@outlook.com.br>
2018-02-05 00:02:05 -0200

    Modulo de Arquivos OK ;

---
+++ Php 7 - Udemy/Modulo-14 - PHP - ARQUIVOS/Aula_01.php
@@ -0,0 +1,21 @@
+<?php
+
+    $name = "img";
+    /* Verificando se um diretorio existe e criando o mesmo */
+       
+        // if(!is_dir($name)) {
+        //     mkdir($name);
+        //     echo "Diretorio {$name} Criado com sucesso ! ";
+        // } else {
+        //    // echo "Diretorio {$name} já existe ! ";
+        //     rmdir($name);
+        //     echo "Já existe diretorio {$name} e já foi removido ! ";
+        // }
+
+
+
+    /* Fim Verificando se um diretorio existe e criando o mesmo */
+
+
+
+?>

adfd7f36107b8ba4ba9b614e8e3802d891bc4399
= <cristian.camargo@outlook.com.br>
2018-02-07 00:05:49 -0200

    Atualização referente as aulas de diretorios

--- Php 7 - Udemy/Modulo-14 - PHP - ARQUIVOS/Aula_02.php
+++ Php 7 - Udemy/Modulo-14 - PHP - ARQUIVOS/Aula_02.php
@@ -1,6 +1,55 @@
 <?php
-    
 
+/* EXEMPLO 01 */
+
+    // // FOPEN = file Open
+
+    // $filename = "logs";
+    // $file = fopen($filename . DIRECTORY_SEPARATOR . "log.txt", "a+");
+
+    // if(!is_dir($filename)) {
+    //     mkdir($filename);
+    // }
+
+    // fwrite($file, date("Y-m-d H:i:s"). "\r\n");
+
+    // fclose($file);
+
+    // echo "Arquivo criado com sucesso ! ";
+/* FIM EXEMPLO 01 */
+
+
+/* EXEMPLO 02 */
+        require_once('DAO/Conn.class.php');
+
+        $sql = new Conn();
+        $usuarios = $sql -> select("SELECT * FROM tb_usuarios ORDER BY deslogin");
+
+        $header = array();
+
+        foreach($usuarios[0] as $key => $value) {
+            array_push($header, ucfirst($key));
+        }
+
+        $file = fopen("logs/usuarios.csv", "w+");
+        fwrite($file, implode(",", $header) . "\r\n");
+        
+        foreach($usuarios as $row) {
+            $data = array();
+
+            foreach($row as $key => $value) {
+                array_push($data, $value);
+            }
+
+            fwrite($file, implode(",", $data) . "\r\n");
+        } // END FOREACH DE LINHA
+        
+        fclose($file);
+
+/* FIM EXEMPLO 01 */
+
+
+    
 
 
 ?>

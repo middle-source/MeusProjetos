fde91ad00aebb0fc4c32ce9a7af0ecae21c9b170
= <cristian.camargo@outlook.com.br>
2018-02-16 00:22:09 -0200

    Atualização v2

---
+++ DesenvolvimentoWeb - Udemy/Modulo-03 - JAVASCRIPT/unidade02/arquivo.js
@@ -0,0 +1,6 @@
+var a = 0000565655545415;
+var b = 6;
+
+var resultado = a + b;
+
+document.write(a + " + " + b  + " = " + resultado);
---
+++ youonbr/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang = "pt-br">
+    <head>
+        <meta charset = "utf-8" />
+        <meta name = "viewport" content = "width=device-width, initial-scale=1.0"/>
+        <meta name = "X-UA-Compatible" content = "ie=edge" />
+        <link rel = "stylesheet" href = "_css/style.css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    </head>
+    <body>
+        <header class = "top">
+            <div class = "img-preloader">
+                <img src = "./_img/logo.png" />
+            <div class = "img">
+        </header>
+        <section class = "body-data">
+            <h1>Em breve, lançamento da nossa loja virtual</h1>
+
+            <h2>Faça seu cadastro garanta e <br> e receba descontos imperdiveis!</h2>
+            <div class = "form-wrapper">
+                <form action = "#" method = "POST">
+                    <input type = "email" class = "lbltext" placeholder = "insira seu e-mail" required/>
+                    <input type = "submit" class "lblsubmit" value = "CADASTRAR" />
+                </form>
+                <br />
+            </div>
+
+            <p>Lançamento dia 01/03</p>
+        </section>
+        <footer>
+            <p>youonbr.com</p>
+        </footer>
+    </body>
+</html>
